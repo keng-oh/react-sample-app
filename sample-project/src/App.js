3c89c6391b769a90ef152c56b49c2473cc6f17bf
keng <ohtakikenji@gmail.com>
2020-03-22 22:42:52 +0900

    hello, jsx

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <h1>Hello, World</h1>;
+  }
 }
+
 
 export default App;
