37b244f2498d3e715401531adc8c6fabd0bf83a8
keng <ohtakikenji@gmail.com>
2020-03-22 22:55:00 +0900

    jsx syntax

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, World</h1>;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar" className="barClass">bar</label>
+        <input type="text" onClick={() => {console.log("clicked!");}} placeholder="click"/>
+        <input type="text" onChange={() => {console.log("changed!");}} placeholder="change"/>
+      </React.Fragment>
+    );
   }
 }
 
