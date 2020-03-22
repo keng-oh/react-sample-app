ebf5203109dc298441851fd4dc7d29dce32683a7
keng <ohtakikenji@gmail.com>
2020-03-22 23:18:13 +0900

    components

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar" className="barClass">bar</label>
-        <input type="text" onClick={() => {console.log("clicked!");}} placeholder="click"/>
-        <input type="text" onChange={() => {console.log("changed!");}} placeholder="change"/>
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar" className="barClass">bar</label>
+//         <input type="text" onClick={() => {console.log("clicked!");}} placeholder="click"/>
+//         <input type="text" onChange={() => {console.log("changed!");}} placeholder="change"/>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+}
 
 export default App;
