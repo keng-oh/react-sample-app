494f3d8695f8fa191d903a61bce9bb4a516e16cd
keng <ohtakikenji@gmail.com>
2020-05-04 21:28:26 +0900

    get event detail

--- sample-project/src/components/events_show.js
+++ sample-project/src/components/events_show.js
@@ -12,6 +12,11 @@ class EventsShow extends Component {
     this.onDeleteClick = this.onDeleteClick.bind(this)
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if (id) this.props.getEvent()
+  }
+
   renderField(field) {
     const { input, label, type, meta: {touched, error} } = field
     return (
@@ -57,8 +62,13 @@ const validate = values => {
 
   return errors
 }
-const mapDispatchToProps = ({ deleteEvent })
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, event }
+}
 
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
+const mapDispatchToProps = ({ deleteEvent, getEvent })
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow)
 )
